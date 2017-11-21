62c5f176d15424a8d42af5c7ef20c66c2de06287
呆呆 <dodonote@sina.com>
2017-11-21 17:33:50 +0800

    py

---
+++ test.py
@@ -0,0 +1,25 @@
+import time
+import pdb
+import sys
+
+print sys.getdefaultencoding()
+
+print range(17501, 100000)
+
+def test():
+    now_time = time.time();
+    print now_time
+    a= 10
+    print a
+    # return
+    a += 1
+    print a
+    # exit
+    b = 'test string'
+    pdb.set_trace
+    print pdb
+    print b
+
+if __name__ == '__main__':
+
+    test()
